4dcdea614c192d37f98c2fa9ae13b2e26cc05230
ciaranmckenna <hello@ciaranmckenna.com>
2018-12-16 17:43:31 +0000

    Creating the project

---
+++ src/fitnessClubManagementSystem/Member.java
@@ -0,0 +1,5 @@
+package fitnessClubManagementSystem;
+
+public class Member {
+
+}
---
+++ src/fitnessClubManagementSystem/MemberDatabaseApp.java
@@ -0,0 +1,10 @@
+package fitnessClubManagementSystem;
+
+public class MemberDatabaseApp {
+
+	public static void main(String[] args) {
+
+		System.out.println("Test");
+	}
+
+}
